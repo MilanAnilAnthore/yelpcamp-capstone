a3a941def57d5d7c381d06a31985ed30c9428e48
MilanAnilAnthore <157392523+MilanAnilAnthore@users.noreply.github.com>
2025-07-13 22:43:38 -0400

    Add image and description to campgrounds
    Updated the Campground model to include an image (String) and changed price to a Number. Seed data now generates random images, descriptions, and prices. The campground show view displays the image and description.

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async () => {
 
     for (i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = await new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur molestiae praesentium reiciendis, provident expedita cum mollitia obcaecati, exercitationem sapiente sed repellat dolore quibusdam inventore rem deserunt ratione voluptas doloremque ad.",
+            price: price
         })
         await camp.save();
     }
