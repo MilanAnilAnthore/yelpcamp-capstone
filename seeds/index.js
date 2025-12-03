3f7d2d51b2cae93e2b525a50644a0792c86c45c1
MilanAnilAnthore <157392523+MilanAnilAnthore@users.noreply.github.com>
2025-12-02 23:37:03 -0500

    Add author field to Campground model and seed data
    Introduced an 'author' field in the Campground schema referencing the User model. Updated the seed script to assign a static author ID to each seeded campground.

--- seeds/index.js
+++ seeds/index.js
@@ -21,6 +21,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = await new Campground({
+            author: '692e03e22c5965e21aadf5b8',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: `https://picsum.photos/400?random=${Math.random()}`,
