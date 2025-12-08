21ce735d7e6825f40cc331eded183835ad1206b7
MilanAnilAnthore <157392523+MilanAnilAnthore@users.noreply.github.com>
2025-12-08 16:13:42 -0500

    Enable image deletion for campgrounds
    Adds support for deleting images from campgrounds. Updates the edit form to allow users to select images for deletion, validates the deleteImages field, and removes images from both the database and Cloudinary.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require('../cloudinary')
 
 
 
@@ -47,6 +48,12 @@ module.exports.updateCampground = async (req, res) => {
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     updatedCampground.images.push(...imgs);
     await updatedCampground.save();
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await updatedCampground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+    }
     req.flash('success', 'Successfully Updated The Campground!')
     res.redirect(`/campgrounds/${updatedCampground._id}`)
 }
